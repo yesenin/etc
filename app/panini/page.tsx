9eff4ccb56691ca6dcc9605ab8eda6f0daa762bd
Anton Yesenin <anton.yesenin@gmail.com>
2026-07-26 12:21:54 +0400

    Panini page

---
+++ app/panini/page.tsx
@@ -0,0 +1,8 @@
+export default function Panini(): React.ReactElement {
+  return (
+    <div>
+      <h1>Welcome to Panini!</h1>
+      <p>This is the main page of the Panini application.</p>
+    </div>
+  );
+}
